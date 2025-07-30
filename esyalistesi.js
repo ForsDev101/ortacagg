8dea20d3f39f2aa64e69907b1bbccbe02d4ce1bd
ForsDev101 <forsdeving@gmail.com>
2025-07-30 13:35:43 +0300

    Create esyalistesi.js

---
+++ esyalistesi.js
@@ -0,0 +1,8 @@
+const esyaListesi = {
+  kilic: { isim: "Demir Kılıç", tur: "silah", hasar: 10 },
+  yari: { isim: "Uzun Yarı", tur: "silah", hasar: 15 },
+  zirh: { isim: "Derin Zırh", tur: "zirh", dayanıklilik: 20 },
+  sifaIksiri: { isim: "Şifa İksiri", tur: "tüketilebilir", iyilesme: 50 }
+};
+
+module.exports = esyaListesi;
